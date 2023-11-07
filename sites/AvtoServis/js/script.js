a1c36a50bbfdc9435384c5bd822d2d5aa21abc28
Igor-Bezoglyuk <igor.bezoglyuk@mail.ru>
2023-11-08 00:35:01 +0300

    script

--- sites/AvtoServis/js/script.js
+++ sites/AvtoServis/js/script.js
@@ -1,1 +1,51 @@
+jQuery(document).ready(function(){
+$(function (){
+    $(".Open").click(function(){
+    $(".Menu2").toggle(500);
+ });
+    $(".Close").click(function(){
+    $(".Menu2").toggle(500);
+ });
+});
+});
 
+/* Прокрутка по нажатию*/
+$(document).ready(function(){
+    $('.Kontent-Silka').click(function(){
+       var target = $(this).attr('href');
+       $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+       return false;
+    });
+ });
+
+$(document).ready(function(){
+    $(".Menu").on("click",".Menu-Silka", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+$(document).ready(function(){
+    $(".Menu2").on("click",".Menu2Silka", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
