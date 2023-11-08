26dd0309c7113a955f7dfbcf9384e9e6cc34a321
Igor-Bezoglyuk <igor.bezoglyuk@mail.ru>
2023-11-08 03:15:59 +0300

    script

--- sites/BatlleZall/js/script.js
+++ sites/BatlleZall/js/script.js
@@ -1,1 +1,43 @@
+jQuery(document).ready(function(){
+$(function (){
+    $("#Open").click(function(){
+    $("#Open").css("opacity","0");
+    $(".MenuPhone:hidden").slideDown().show(500);
+ });
+    $(".Close").click(function(){
+    $("#Open").css("opacity","1");
+    $(".MenuPhone").slideUp().hide(500);
+ });
+});
+});
 
+
+/*______________________________________ButtonTop___________________________________________*/
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#KnopkaTop').fadeIn();
+ 
+} else {
+ 
+$('#KnopkaTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#KnopkaTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
+
+/*______________________________________С подключением Jqery в HTML_____________________________*/
+
+/*<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>*/
